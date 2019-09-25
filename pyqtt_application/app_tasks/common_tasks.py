9e1f19a97c707135a5a6308d8ca50b219f31f68a
F59JBP0 <ee.jbiondi@ficosa.com>
2019-09-25 14:41:11 +0200

    Fix error on stop_task

--- pyqtt_application/app_tasks/common_tasks.py
+++ pyqtt_application/app_tasks/common_tasks.py
@@ -2,12 +2,13 @@ from pyqtt_application.extensions import celery
 
 
 @celery.task(bind=True)
-def stop_task(task_id: str):
+def stop_task(self, task_id: str):
     """
     Stop a task by its given id.
 
     Args:
+        self:
         task_id: Id of the task to be stopped.
 
     """
-    celery.control.revoke(task_id, terminate=True)
+    celery.control.revoke([task_id], terminate=True)
