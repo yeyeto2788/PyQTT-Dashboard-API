afd1ec6852697ed61dca56de0cac33c868bfe587
F59JBP0 <ee.jbiondi@ficosa.com>
2019-09-25 14:41:44 +0200

    Lint the code to make it more readable.

--- pyqtt_application/app.py
+++ pyqtt_application/app.py
@@ -25,7 +25,9 @@ def authenticate(username, password):
         User object.
     """
     user = User.query.filter_by(username=username).first()
+
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+
         return user
 
 
