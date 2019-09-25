90d33cca93f84795a094a28ed51fff2985fd66b2
F59JBP0 <ee.jbiondi@ficosa.com>
2019-09-25 14:45:28 +0200

    structure the code and add more info. Change the name of the real db.

--- pyqtt_application/config.py
+++ pyqtt_application/config.py
@@ -5,16 +5,26 @@ import os
 
 __key = os.urandom(16)  # Change this to a fix string.
 
-__default_db = 'sqlite:///' + os.path.join(os.getcwd(), 'test.db')
+__default_db = 'sqlite:///' + os.path.join(os.getcwd(), 'pyqtt.db')
 
+# Flask configuration.
+SECRET_KEY = __key
+
+# SQLAlchemy configuration.
 SQLALCHEMY_DATABASE_URI = os.getenv("PYQTT_DATABASE_URL", __default_db)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSON_SORT_KEYS = False
-JWT_SECRET_KEY = __key
-SECRET_KEY = __key
+
+# JWT configuration.
+JWT_SECRET_KEY = os.getenv("JWT_SECRET", 'dummy_secret_jwt')
+JWT_ALGORITHM = "HS256"
+
+# Celery configuration.
 CELERY_BROKER_URL = os.getenv("CELERY_URL", "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_BACKEND_URL", "redis://localhost:6379/0")
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_IMPORTS = ['pyqtt_application.app_tasks.mqtt_tasks']
+CELERY_IMPORTS = [
+    'pyqtt_application.app_tasks.mqtt_tasks',
+    'pyqtt_application.app_tasks.common_tasks']
 CELERY_ACCEPT_CONTENT = ['json']
